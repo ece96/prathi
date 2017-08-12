1d219076cb08934afbce23728b2ea38be8b8c080
ece96 <30343336+ece96@users.noreply.github.com>
2017-08-12 16:16:53 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ int n;
+ printf("\n enter the given number");
+ scanf("%d",&n);
+ if(n/2==0)
+  printf("\n the given number is even");
+ else
+   printf(" \n the given numbher is not even");
+ getch();
+ }
